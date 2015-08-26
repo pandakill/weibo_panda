68b431ccbcd7313c5385a41dc72f341433f4fb1c
panda <asdf@qq.com>
2015-08-26 20:05:21 +0800

    新建写评论的activity

---
+++ app/src/main/java/com/panda/pweibo/activity/WriteCommentActivity.java
@@ -0,0 +1,18 @@
+package com.panda.pweibo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.panda.pweibo.R;
+
+/**
+ * Created by Administrator on 2015/8/26:19:45.
+ */
+public class WriteCommentActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_write_status);
+    }
+}
